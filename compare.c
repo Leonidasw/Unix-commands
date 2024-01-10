059b479e770a2615f993b3fd5df84d98c68d8df9
Leonidasw <98226972+Leonidasw@users.noreply.github.com>
2024-01-10 23:23:16 +0100

    Add files via upload

---
+++ compare.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+ 
+// defining main with arguments
+int main(int argc, char* argv[])
+{
+    char buffer[50];
+    char buffer2[50];
+    FILE *fp;
+    FILE *fp2;
+    argc -= 1;
+    if (argc <= 2) {
+        if (access(argv[1], F_OK) == 0 && access(argv[2], F_OK) == 0 ) {
+            printf("You have entered %d arguments:\n", argc);
+            fp = fopen(argv[1], "r");
+            fp2 = fopen(argv[2], "r");
+            while (fgets(buffer,50,fp) != NULL && fgets(buffer2,50,fp2) != NULL){
+                continue;
+            };
+            if (strcmp(buffer,buffer2) == 0){
+                    printf("%s\n", "same");
+                }
+            else {
+                printf("different");
+            };
+
+        } 
+        else {
+            if (access(argv[1], F_OK) == 1) {
+                printf("%s is an inexistant file", argv[1]);
+            }
+            else {
+                printf("%s is an inexistant file", argv[2]);
+            }
+        }
+
+
+    }
+    else {
+        printf("too many arguments");
+    }
+    return 0;
+}
---
+++ copy.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+ 
+// defining main with arguments
+
+const char *get_filename_ext(const char *filename) {
+    if (strrchr(filename, '.') != NULL) {
+        const char *dot = strrchr(filename, '.');
+        if(!dot || dot == filename) return "";
+        return dot + 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+
+    char buffer[50];
+    char buffer2[50];
+    char result[100];
+    FILE *fp;
+    FILE *fp2;
+    argc -= 1;
+    if (argc == 2) {
+        if (access(argv[1], F_OK) == 0 && access(argv[2], F_OK) == 0 ) {
+            printf("You have entered %d arguments:\n", argc);
+            fp = fopen(argv[1], "r");
+            fp2 = fopen(argv[2], "w");
+            while (fgets(buffer,50,fp) != NULL) {
+                fputs(buffer, fp2);
+            };
+        } 
+        else {
+            if (access(argv[1], F_OK) == 1) {
+                printf("%s is an inexistant file", argv[1]);
+            }
+            else {
+                if (get_filename_ext(argv[1]) != 0) {
+                    sprintf(result,"%s%s%s",argv[2],".",get_filename_ext(argv[1]));
+                    fp2 = fopen(result, "w");
+                    printf("You have entered %d arguments:\n", argc);
+                    fp = fopen(argv[1], "r");
+                    while (fgets(buffer,50,fp) != NULL) {
+                        fputs(buffer, fp2);
+                    };
+                }
+                else {
+                    fp2 = fopen(argv[2], "w");
+                    printf("You have entered %d arguments:\n", argc);
+                    fp = fopen(argv[1], "r");
+                    while (fgets(buffer,50,fp) != NULL) {
+                        fputs(buffer, fp2);
+                    };
+                };
+            };
+        };
+
+    }
+    else if(argc > 2){
+        printf("too many arguments");
+    }
+    else {
+        printf("too few arguments");
+    }
+    
+    return 0;
+}
