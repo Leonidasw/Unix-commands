72332c30e795c567a44bc2e22d2bd57d5f348808
Leonidasw <98226972+Leonidasw@users.noreply.github.com>
2024-01-12 00:25:26 +0100

    Add files via upload

---
+++ cat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+ 
+// defining main with arguments
+int main(int argc, char* argv[])
+{
+    char buffer[50];
+    FILE *fp;
+    if (argc >= 1) {
+        for (int i = 1; i < argc; i++) {
+            if (access(argv[i], F_OK) == 0) {
+                fp = fopen(argv[i], "r");
+                while (fgets(buffer,50,fp) != NULL){
+                    printf("%s", buffer);
+                };
+                printf("\n");
+            } 
+            else {
+                if (access(argv[i], F_OK) != 0) {
+                    printf("%s is an inexistant file", argv[i]);
+                }
+            }
+        }
+    }
+    else {
+        printf("not enough arguments");
+    };
+    return 0;
+}
