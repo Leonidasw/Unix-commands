435d5e22a6fe2307bdfcd2e32156fb4025380de8
Leonidasw <98226972+Leonidasw@users.noreply.github.com>
2024-01-12 15:53:20 +0100

    Update yes.c

--- yes.c
+++ yes.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-int main() {
-    while (1) {
-        printf("y\n");
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        while (1) {
+            printf("y\n");
+        }
+    }
+    else {
+        while (1) {
+            printf("%s\n",argv[1]);
+        }
     }
     return 0;
 }
